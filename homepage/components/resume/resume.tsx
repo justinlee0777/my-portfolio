fb9e10b3237d877684668d3f3ff3a8fcf698bd89
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-02-22 14:27:33 -0500

    Adding Resume page

---
+++ homepage/components/resume/resume.tsx
@@ -0,0 +1,31 @@
+import styles from "./resume.module.scss";
+
+export default function Resume(): JSX.Element {
+  return (
+    <>
+      <p className={styles.line}>
+        I am a web developer. I built this site using React and Next.js, and I
+        host it myself. All for fun.
+      </p>
+      <p className={styles.line}>
+        I have worked with{" "}
+        <a href="https://www.unboundcommerce.com" rel="noopener noreferrer">
+          Unbound Commerce
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.sap.com/products/crm/e-commerce-platforms.html"
+          rel="noopener noreferrer"
+        >
+          SAP
+        </a>{" "}
+        to make B2B websites a bit more complex than this one.
+      </p>
+      <p className={styles.line}>Though these are more fun.</p>
+      <p className={styles.line}>
+        I loathe handing out résumés. The work is always more interesting.
+        Contact me at _blank_ if the site amuses you or for anything else.
+      </p>
+    </>
+  );
+}
--- homepage/index.tsx
+++ homepage/index.tsx
@@ -1,11 +1,17 @@
 import DeveloperDescription from "./components/developer-description/developer-description";
+import Resume from "./components/resume/resume";
 import Slide from "./components/slide/slide";
 
 function HomePage() {
   return (
-    <Slide>
-      <DeveloperDescription />
-    </Slide>
+    <>
+      <Slide>
+        <DeveloperDescription />
+      </Slide>
+      <Slide>
+        <Resume />
+      </Slide>
+    </>
   );
 }
 
