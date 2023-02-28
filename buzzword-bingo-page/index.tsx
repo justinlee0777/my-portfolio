3ad0800595910f4d293edd2d13b3970eb41cff93
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-02-28 16:09:08 -0500

    Adding explanatory text for buzzword bingo

--- buzzword-bingo-page/index.tsx
+++ buzzword-bingo-page/index.tsx
@@ -1,3 +1,5 @@
+import styles from './index.module.scss';
+
 import { BuzzwordBingo } from 'buzzword-bingo-generator';
 
 import Slide from '../components/slide/slide';
@@ -5,7 +7,22 @@ import Slide from '../components/slide/slide';
 export default function BuzzwordBingoPage() {
   return (
     <Slide>
-      <BuzzwordBingo />
+      <>
+        <h1>Buzzword bingo</h1>
+        <div className={styles.explanation}>
+          <p>
+            Buzzword bingo is typically reserved for stock phrases said during
+            an event, but I personally use it for clichés and themes in general.
+          </p>
+          <p>
+            It's best used for things that you're half-interested in, as you
+            have a pretty good idea of what will happen, such as Disney and Fast
+            and the Furious movies, and Hell's Kitchen.
+          </p>
+          <p>Though I use it for things I enjoy as well.</p>
+        </div>
+        <BuzzwordBingo />
+      </>
     </Slide>
   );
 }
