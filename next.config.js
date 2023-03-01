fda2ce2cc50c64a3c2e16a490bc4ea423ca54f3a
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-02-28 23:55:51 -0500

    Adding script to analyze react bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const CopyPlugin = require('copy-webpack-plugin');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = {
+const config = {
   webpack: (config) => {
     return {
       ...config,
@@ -14,3 +17,5 @@ module.exports = {
     };
   },
 };
+
+module.exports = withBundleAnalyzer(config);
