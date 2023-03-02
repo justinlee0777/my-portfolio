72b4a2c69faa431e504fb5666ab25994f2de541f
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-03-02 13:39:55 -0500

    Testing loadFont() using Jest mocks

--- config/load-font.function.test.ts
+++ config/load-font.function.test.ts
@@ -1,12 +1,56 @@
 import { Font } from './font.enum';
-import { getFontUrl } from './load-font.function';
+import { getFontUrl, loadFont } from './load-font.function';
 
-test('gets the font URL if the font is not provided in the browser', () => {
-  expect(() => getFontUrl(Font.ARIAL)).toThrow('No font url for Arial');
+describe('loadFont()', () => {
+  let savedFont: Font;
+  let savedUrl: string;
 
-  expect(() => getFontUrl(Font.COMIC_SANS_MS)).toThrow(
-    'No font url for Comic Sans MS'
-  );
+  let addedFonts = [];
 
-  expect(() => getFontUrl(Font.ROBOTO)).toBeTruthy();
+  let loadCalled = false;
+  class MockFontFace {
+    constructor(font: Font, url: string) {
+      savedFont = font;
+      savedUrl = url;
+    }
+
+    load = jest.fn().mockImplementationOnce(() => {
+      loadCalled = true;
+      return Promise.resolve(undefined);
+    });
+  }
+  global.FontFace = <any>MockFontFace;
+
+  const mockDocument: any = {
+    fonts: {
+      add: jest
+        .fn()
+        .mockImplementationOnce((fontFace) => addedFonts.push(fontFace)),
+    },
+  };
+
+  global.document = mockDocument;
+
+  test('loads a font', async () => {
+    await loadFont(Font.ROBOTO);
+
+    expect(savedFont).toBe(Font.ROBOTO);
+    expect(savedUrl).toMatch(/url\(.*\)/);
+
+    expect(addedFonts.length).toBe(1);
+
+    expect(loadCalled).toBe(true);
+  });
+});
+
+describe('getFontUrl()', () => {
+  test('gets the font URL if the font is not provided in the browser', () => {
+    expect(() => getFontUrl(Font.ARIAL)).toThrow('No font url for Arial');
+
+    expect(() => getFontUrl(Font.COMIC_SANS_MS)).toThrow(
+      'No font url for Comic Sans MS'
+    );
+
+    expect(() => getFontUrl(Font.ROBOTO)).toBeTruthy();
+  });
 });
