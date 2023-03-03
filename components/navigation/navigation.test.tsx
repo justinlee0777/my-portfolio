8d954e57e20483adea533a7fd0d46b36024d343a
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-03-02 21:00:47 -0500

    Test for Navigation component

---
+++ components/navigation/navigation.test.tsx
@@ -0,0 +1,50 @@
+import { cleanup, render, screen } from '@testing-library/react';
+
+import { Navigation } from './navigation';
+
+describe('<Navigation/>', () => {
+  afterEach(() => {
+    cleanup();
+    jest.resetModules();
+  });
+
+  test('renders', () => {
+    const renderResult = render(
+      <Navigation
+        links={[
+          {
+            displayName: 'Foo',
+            url: '/foo',
+            isHome: true,
+          },
+          {
+            displayName: 'Bar',
+            url: '/bar',
+          },
+          {
+            displayName: 'Baz',
+            url: '/baz',
+          },
+        ]}
+      />
+    );
+
+    expect((renderResult.container.childNodes[0] as HTMLElement).tagName).toBe(
+      'NAV'
+    );
+
+    const skipLink = screen.queryByText('Skip to main content');
+    expect(skipLink).toBeTruthy();
+    expect(skipLink.tagName).toBe('A');
+
+    const fooLink = screen.queryByText('Foo');
+    expect(fooLink).toBeTruthy();
+    expect(fooLink.classList.toString()).toBe('navigationLink homeLink');
+
+    const barLink = screen.queryByText('Bar');
+    expect(barLink).toBeTruthy();
+
+    const bazLink = screen.queryByText('Baz');
+    expect(bazLink).toBeTruthy();
+  });
+});
