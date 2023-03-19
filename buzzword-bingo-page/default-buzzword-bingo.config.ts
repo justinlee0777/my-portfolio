9df009a665d551557262726445a5d106f6b92752
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-03-19 00:14:30 -0400

    Upgrading buzzword-bingo and removing warning for mobile

--- buzzword-bingo-page/default-buzzword-bingo.config.ts
+++ buzzword-bingo-page/default-buzzword-bingo.config.ts
@@ -11,7 +11,6 @@ export const defaultBuzzwordBingoConfig: BuzzwordBingoConfig = {
       'Buzzword bingo is typically reserved for stock phrases said during an event, but I personally use it for clichés and themes in general.',
       "It's best used for things that you're half-interested in, as you have a pretty good idea of what will happen, such as Disney and Fast and the Furious movies, and Hell's Kitchen.",
       'Though I use it for things I enjoy as well.',
-      'For mobile, the 5 x 5 sheet will be too big and cut off.',
     ],
   },
 };
