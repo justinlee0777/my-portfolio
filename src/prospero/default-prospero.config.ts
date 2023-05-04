7bae845f0ebc0440760a6a6e1553028638a5bd0a
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-05-03 22:39:07 -0400

    WIP: Correcting Prospero description

--- src/prospero/default-prospero.config.ts
+++ src/prospero/default-prospero.config.ts
@@ -9,7 +9,7 @@ const defaultProsperoConfig: ProsperoConfig = {
   textContent: {
     header: 'Prospero: Render text on the web as a book',
     description:
-      'Prospero paginates text on the server and the pages on the web in the form of an e-book.\nClick on the book and flip through the book with the keyboard arrow keys or swiping.',
+      'Prospero paginates text on the server and renders the pages on the web in the form of an e-book.\nClick on the book and flip through the book with the keyboard arrow keys or swiping.',
   },
 };
 
