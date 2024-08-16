a2f15ea33f27bf75a76463ef40186b1eddc11b0e
Justin Lee <justinlee0777@gmail.com>
2024-08-16 13:10:11 -0400

    trying something again

--- src/api/google/translate.client.ts
+++ src/api/google/translate.client.ts
@@ -1,5 +1,4 @@
 import { TranslationServiceClient } from '@google-cloud/translate';
-import { getVercelOidcToken } from '@vercel/functions/oidc';
 import { ExternalAccountClient } from 'google-auth-library';
 
 export default function getGoogleTranslateClient() {
@@ -9,19 +8,8 @@ export default function getGoogleTranslateClient() {
     GPC_WORKLOAD_IDENTITY_POOL_ID,
     GPC_WORKLOAD_IDENTITY_POOL_PROVIDER_ID,
     GCP_SERVICE_ACCOUNT_EMAIL,
+    VERCEL_OIDC_TOKEN
   } = process.env;
-
-  console.log('auth config', {
-    type: 'external_account',
-    audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GPC_WORKLOAD_IDENTITY_POOL_ID}/providers/${GPC_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
-    subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
-    token_url: 'https://sts.googleapis.com/v1/token',
-    service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
-    subject_token_supplier: {
-      // Use the Vercel OIDC token as the subject token
-      getSubjectToken: getVercelOidcToken,
-    },
-  });
 
   const authClient = ExternalAccountClient.fromJSON({
     type: 'external_account',
@@ -31,7 +19,7 @@ export default function getGoogleTranslateClient() {
     service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
     subject_token_supplier: {
       // Use the Vercel OIDC token as the subject token
-      getSubjectToken: getVercelOidcToken,
+      getSubjectToken: async () => VERCEL_OIDC_TOKEN,
     },
   });
 
