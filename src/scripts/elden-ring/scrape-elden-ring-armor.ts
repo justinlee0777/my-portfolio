3bb6198fa7c505b46aecefd3ca21e89745890351
Justin Lee <justinlee0777@gmail.com>
2024-08-30 12:41:20 -0400

    Scrapes for helms, gauntlets and greaves. Chest armor is not recorded the same way on the fandom site

---
+++ src/scripts/elden-ring/scrape-elden-ring-armor.ts
@@ -0,0 +1,110 @@
+import { chromium, Locator } from 'playwright';
+import { EldenRingEmbeddings } from '../../models/elden-ring-embeddings.model';
+import EldenRingScrapeResults from './elden-ring-scrape-results.interface';
+
+export default async function* scrapeEldenRingArmor(): AsyncGenerator<
+  Array<EldenRingScrapeResults>
+> {
+  const browser = await chromium.launch();
+
+  const context = await browser.newContext();
+
+  const page = await context.newPage();
+
+  const baseUrl = `https://eldenring.fandom.com`;
+
+  const pageConfigs: Array<{
+    url: string;
+    itemType: EldenRingEmbeddings['itemType'];
+  }> = [
+    { url: `${baseUrl}/wiki/Head`, itemType: 'helm' },
+    // below doesn't work... use this instead https://eldenring.fandom.com/wiki/Category:Chest
+    // { url: `${baseUrl}/wiki/Chest`, itemType: 'chest armor' },
+    { url: `${baseUrl}/wiki/Arms`, itemType: 'gauntlets' },
+    { url: `${baseUrl}/wiki/Legs`, itemType: 'leg armor' },
+  ];
+
+  for (const { url, itemType } of pageConfigs) {
+    const content: Array<EldenRingScrapeResults> = [];
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    const tableRowSelectors = await page
+      .locator('.wds-tabber')
+      .locator('.wikia-gallery-item')
+      .locator('a')
+      .all();
+
+    const batchedTableRowSelectors: Array<Array<Locator>> = [];
+
+    let pageNumber = 0;
+
+    while (pageNumber < tableRowSelectors.length) {
+      const size = 5;
+      const nextPageNumber = pageNumber + size;
+
+      batchedTableRowSelectors.push(
+        tableRowSelectors.slice(pageNumber, nextPageNumber)
+      );
+
+      pageNumber += size;
+    }
+
+    for (const batch of batchedTableRowSelectors) {
+      const descriptions = await Promise.all(
+        batch.map(async (selector) => {
+          try {
+            const weaponPage = await context.newPage();
+
+            const path = await selector.getAttribute('href');
+
+            await weaponPage.goto(`${baseUrl}/${path}`, {
+              waitUntil: 'domcontentloaded',
+            });
+
+            const paragraphs = await weaponPage
+              .locator('h2:has-text("Description") + div')
+              .locator('.wds-tabber')
+              .locator('.wds-is-current.wds-tab__content')
+              .locator('p')
+              .all();
+
+            const itemName = await weaponPage.locator('h1').textContent();
+            const textContent = await Promise.all(
+              paragraphs.map((paragraph) => paragraph.textContent())
+            );
+
+            await weaponPage.close();
+
+            const trimmedItemName = itemName.trim();
+
+            return {
+              itemName: trimmedItemName,
+              chunks: [
+                textContent
+                  .map((text) => text.trim())
+                  .filter((text) => text.length !== 0)
+                  .join('\n'),
+              ],
+              itemType,
+            };
+          } catch (error) {
+            console.log(error);
+            return null;
+          }
+        })
+      );
+
+      content.push(
+        ...descriptions.filter((description) => description?.chunks.at(0))
+      );
+
+      console.log({
+        lastContent: content.at(-1),
+      });
+    }
+
+    await browser.close();
+
+    yield content;
+  }
+}
--- src/scripts/elden-ring/upload-elden-ring-embeddings.ts
+++ src/scripts/elden-ring/upload-elden-ring-embeddings.ts
@@ -3,38 +3,38 @@ import 'dotenv/config';
 import getOpenAIApi from '../../api/openai/open-ai.client';
 import { EldenRingEmbeddingsModel } from '../../models/elden-ring-embeddings.model';
 import connectToMongoDB from '../../page-utils/prospero/connect-to-mongodb.function';
-import scrapeEldenRingWeapons from './scrape-elden-ring-weapons';
+import scrapeEldenRingArmor from './scrape-elden-ring-armor';
 
 async function uploadEldenRingEmbeddings() {
-  const content = await scrapeEldenRingWeapons();
+  for await (const content of scrapeEldenRingArmor()) {
+    for (const { itemName, chunks, itemType } of content) {
+      console.log(`working on ${itemName}...`);
+      const { embeddings } = getOpenAIApi();
 
-  for (const { itemName, chunks, itemType } of content) {
-    console.log(`working on ${itemName}...`);
-    const { embeddings } = getOpenAIApi();
+      const generatedEmbeddings = await embeddings.create({
+        model: 'text-embedding-3-small',
+        input: chunks,
+      });
 
-    const generatedEmbeddings = await embeddings.create({
-      model: 'text-embedding-3-small',
-      input: chunks,
-    });
+      await connectToMongoDB();
 
-    await connectToMongoDB();
-
-    await EldenRingEmbeddingsModel.bulkWrite(
-      generatedEmbeddings.data.map((generatedEmbedding, i) => ({
-        updateOne: {
-          filter: { itemName, text: chunks[i] },
-          update: {
-            $set: {
-              itemName,
-              text: chunks[i],
-              embedding: generatedEmbedding.embedding,
-              itemType,
+      await EldenRingEmbeddingsModel.bulkWrite(
+        generatedEmbeddings.data.map((generatedEmbedding, i) => ({
+          updateOne: {
+            filter: { itemName, text: chunks[i] },
+            update: {
+              $set: {
+                itemName,
+                text: chunks[i],
+                embedding: generatedEmbedding.embedding,
+                itemType,
+              },
             },
+            upsert: true,
           },
-          upsert: true,
-        },
-      }))
-    );
+        }))
+      );
+    }
   }
 }
 
