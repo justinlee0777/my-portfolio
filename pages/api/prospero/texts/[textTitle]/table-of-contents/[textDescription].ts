a28d3a00e22bd4299bf54ffba95c982384247e37
Justin Lee <justinlee0777@gmail.com>
2024-12-31 10:38:57 -0500

    this probably explains why the table of contents would occasionally not work

--- pages/api/prospero/texts/[textTitle]/table-of-contents/[textDescription].ts
+++ pages/api/prospero/texts/[textTitle]/table-of-contents/[textDescription].ts
@@ -1,11 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ProsperoTableOfContentsModel } from '../../../../../../src/models/prospero-table-of-contents.model';
+import connectToMongoDB from '../../../../../../src/page-utils/prospero/connect-to-mongodb.function';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
+    await connectToMongoDB();
+
     const result = await ProsperoTableOfContentsModel.findOne({
       textTitle: req.query.textTitle,
       textDescription: req.query.textDescription,
