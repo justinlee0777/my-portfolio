06f56527318ff6c38d7d29d224c8afbc98f06134
Justin Lee <justinlee0777@gmail.com>
2026-03-16 13:23:36 -0400

    npm audit fix and removing google translate function + dep

--- src/api/google/translate.client.ts
+++
@@ -1,6 +0,0 @@
-import { TranslationServiceClient } from '@google-cloud/translate';
-import getGCPCredentials from '../../utils/get-gcpc-credentials.function';
-
-export default function getGoogleTranslateClient() {
-  return new TranslationServiceClient(getGCPCredentials());
-}
--- src/api/google/translate.function.ts
+++ src/api/google/translate.function.ts
@@ -1,5 +1,3 @@
-import getGoogleTranslateClient from './translate.client';
-
 /**
  * @returns the strings translated preserved in the order they were sent (as I trust Google to preserve ordinality).
  */
@@ -8,18 +6,5 @@ export default async function translate(
   targetLang: string,
   sourceLang = 'en'
 ): Promise<Array<string>> {
-  const googleTranslateClient = getGoogleTranslateClient();
-
-  const { GCP_PROJECT_ID } = process.env;
-
-  const response = await googleTranslateClient.translateText({
-    contents: strings,
-    targetLanguageCode: targetLang,
-    sourceLanguageCode: sourceLang,
-    parent: `projects/${GCP_PROJECT_ID}/locations/global`,
-  });
-
-  return response[0].translations!.map(
-    (translation) => translation.translatedText ?? ''
-  );
+  throw new Error('No longer implemented.');
 }
