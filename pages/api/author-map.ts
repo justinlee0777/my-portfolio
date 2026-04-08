29aa040a48ac0db4e11cc732068a451024fea215
Justin Lee <justinlee0777@gmail.com>
2026-04-08 10:26:52 -0400

    gunzipping API endpoint response

--- pages/api/author-map.ts
+++ pages/api/author-map.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { promisify } from 'util';
+import { gzip } from 'zlib';
 
 import { getAuthorMapData } from '../../src/utils/author-map/utils';
+
+const gzipAsync = promisify(gzip);
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,8 +12,13 @@ export default async function handler(
 ) {
   if (req.method === 'GET') {
     const authorMapData = await getAuthorMapData();
+    const compressed = await gzipAsync(JSON.stringify(authorMapData));
 
-    res.status(200).json(authorMapData);
+    res
+      .setHeader('Content-Encoding', 'gzip')
+      .setHeader('Content-Type', 'application/json')
+      .status(200)
+      .send(compressed);
   } else {
     res.status(404).end();
   }
